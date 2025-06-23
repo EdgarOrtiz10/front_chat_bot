3ed6d689d164acb4b202bd205cf75793f2dd004c
EdgarOrtiz10 <2120eaoe@gmail.com>
2025-06-23 18:58:25 -0500

    V1.1

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
-from app.routes import chat  # asegúrate de tener app/routes/chat.py
+from app.routes import chat
 
 app = FastAPI()
 
+# Montar archivos estáticos (css, js)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Incluir rutas de la app
 app.include_router(chat.router)
 
+# Redirigir raíz hacia el frontend
 @app.get("/")
 def root():
     return RedirectResponse(url="/chat")
