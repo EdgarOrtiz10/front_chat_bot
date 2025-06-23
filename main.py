abaf7414cd098cc2009d48c72eb075085e4112fa
EdgarOrtiz10 <2120eaoe@gmail.com>
2025-06-23 18:30:39 -0500

    V1.0

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+from app.routes import chat
+
 app = FastAPI()
-@app.get('/')
-def root(): return {'message': 'Chatbot en Render'}
+
+# Montar archivos estáticos (CSS/JS)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Incluir rutas
+app.include_router(chat.router)
+
+# Redirigir / hacia /chat
+@app.get("/")
+def root():
+    return RedirectResponse(url="/chat")
